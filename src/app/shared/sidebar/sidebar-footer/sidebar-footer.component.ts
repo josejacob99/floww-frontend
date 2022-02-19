72c492a28616d274b5b1901e9fee7136816a1c5f
josejacob <josejacob99@gmail.com>
2022-02-19 22:07:00 +0530

    Feature: Added correct build version

--- src/app/shared/sidebar/sidebar-footer/sidebar-footer.component.ts
+++ src/app/shared/sidebar/sidebar-footer/sidebar-footer.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+declare function require(moduleName: string): any;
 @Component({
   selector: 'app-sidebar-footer',
   templateUrl: './sidebar-footer.component.html',
   styleUrls: ['./sidebar-footer.component.scss']
 })
 export class SidebarFooterComponent implements OnInit {
-
+  public version: string = `v${require('../../../../../package.json').version}`;
   constructor() { }
 
   ngOnInit(): void {
