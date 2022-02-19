54909c9afa4c2ce91474ea3e3bbe8c9a429431e3
josejacob <josejacob99@gmail.com>
2022-02-19 23:05:46 +0530

    Added auth spinner

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
 import { AuthService } from 'src/app/core/service/auth.service';
+import { UIStateService } from 'src/app/core/service/state/ui-state.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,11 +10,14 @@ import { AuthService } from 'src/app/core/service/auth.service';
 })
 export class AuthComponent {
 
-  constructor(private socialAuthService: SocialAuthService, private authService: AuthService) { }
+  constructor(private socialAuthService: SocialAuthService, private authService: AuthService, private uiState: UIStateService) { }
 
   loginWithGoogle(): void {
+    this.uiState.showSpinner();
     this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then(data => {
-      this.authService.loginWithGoogleidToken(data.idToken).subscribe();
+      this.authService.loginWithGoogleidToken(data.idToken).subscribe(() => {
+        this.uiState.hideSpinner();
+      });
     });
   }
 }
