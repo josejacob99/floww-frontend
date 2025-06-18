bd14b3e37d8602d13e490fa2c226fb86c79307a7
JoseJ <josejacob99@gmail.com>
2025-06-18 17:47:04 +0530

    revert gkey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ const socialAuthConfig = {
       {
         id: GoogleLoginProvider.PROVIDER_ID,
         provider: new GoogleLoginProvider(
-          '1095327336712-jr6nno76qdo9njcpho21p5rat1puqq7o.apps.googleusercontent.com'
+          ''  
         )
       }
     ],
